5605b790a34cc8d925f9731d292ca0350efd8589
Victor <victor.baz92@gmail.com>
2025-02-07 19:26:12 +0100

    shader + avancement sinus

--- ProjectRadioGroup4/Assets/Scripts/Controller/RadioController.cs
+++ ProjectRadioGroup4/Assets/Scripts/Controller/RadioController.cs
@@ -12,14 +12,12 @@ public class RadioController : MonoBehaviour
     [Header("Canvas")]
     [SerializeField] private Canvas canvaRadio;
 
-    [Header("Frequence Radio")] 
-    [SerializeField] private int frequencyRadio;
-
-    [SerializeField] private Slider sliderHz;
-
-    [SerializeField] private TMP_Text frequenceText;
+    [Header("Frequence Radio")] [SerializeField]
+    private RawImage imageRadio;
+    private Material mat;
     
-    private bool isRadioOpen = false;
+    
+    
     
     private void Awake()
     {
@@ -31,65 +29,19 @@ public class RadioController : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        canvaRadio.enabled = false;
-        sliderHz.onValueChanged.AddListener(UpdateFrequencyFromSlider);
+        mat = imageRadio.material;
     }
 
-    private void Update()
+    public void AddWave(float i)
     {
-       OpenClosedRadio();
-       UpdateHzMouse();
-       UpdateHzSlider();
+        var value = mat.GetFloat("_waves_Amount");
+        mat.SetFloat("_waves_Amount", value + i);
+    }
+
+    public void AddWaveAmp(float i)
+    {
+        var value = mat.GetFloat("_waves_Amp");
+        mat.SetFloat("_waves_Amp",value + i);
     }
     
-    private void UpdateFrequencyFromSlider(float value)
-    {
-        frequencyRadio = (int)value;
-        UpdateUiHz();
-    }
-
-    
-    private void OpenClosedRadio()
-    {
-        if (!Input.GetKeyDown(KeyCode.F)) return;
-        switch (isRadioOpen)
-        {
-            case false : canvaRadio.enabled = true;
-                isRadioOpen = true;
-                break;
-            case true : canvaRadio.enabled = false;
-                isRadioOpen = false;
-                break;
-        } 
-    }
-
-    private void UpdateHzMouse()
-    {
-        if (!isRadioOpen) return;
-        switch (Input.GetAxisRaw("Mouse ScrollWheel"))
-        {
-            case > 0 :
-                frequencyRadio++;
-                break;
-            case < 0 :
-                frequencyRadio--;
-                break;
-        }
-        UpdateUiHz();
-    }
-
-    private void UpdateUiHz()
-    {
-        frequencyRadio = Mathf.Clamp(frequencyRadio, 20, 20000);
-        frequenceText.text = frequencyRadio.ToString() + " Hz";
-    }
-    
-    private void UpdateHzSlider()
-    {
-        if (!isRadioOpen) return;
-        if (!Mathf.Approximately(sliderHz.value, frequencyRadio)) 
-        {
-            sliderHz.value = frequencyRadio;
-        }
-    }
 }
