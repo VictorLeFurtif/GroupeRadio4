33b731e276afd5e064db4caf2fb0779f97957299
Ethan <ethanchaintron@gmail.com>
2025-03-31 15:53:33 +0200

    Dungeon Manager (génération semi-procédurale)

--- ProjectRadioGroup4/Assets/Scripts/GENERATOR/DungeonGenerator.cs
+++ ProjectRadioGroup4/Assets/Scripts/GENERATOR/DungeonGenerator.cs
@@ -1,15 +1,15 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
+//MADE BY ETHAN GD_TECH
+//victor a dit que mon code est super UwU
+//j'ai pas utilisé chat gpt je suis super intelligent
 public class DungeonGenerator : MonoBehaviour
 {
-    ///////////////////////
-    ////// VARIABLES //////
-    ///////////////////////
-    
-    [SerializeField] private  List<string> dungeon; // Liste des salles du donjon
+    [SerializeField] private int curentFloor;
+    [SerializeField] private List<string> dungeon; // Liste des salles du donjon
     
     private readonly List<List<string>> floors = new(); // Liste des étages
     
@@ -20,42 +20,38 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private  List<string> levels4;
     [SerializeField] private  List<string> levels5;
 
-    [SerializeField] private int seed; // Graine de génération du pseudo-aléatoire
+    [SerializeField] private int seed;
     
-    // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
         Random.InitState(seed);
         floors.AddRange(new List<List<string>> { levels1, levels2, levels3, levels4, levels5 }); // Initialise la liste des étages avec les listes des niveaux
         GenerateDungeon();
+        GoToNextFloor();
     }
     
-    private void Update()
-    {
-        // DEBUG ONLY
-        // Permet de recommencer la génération
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            RestartGeneration();
-        }
-    }
-
-    // ReSharper disable Unity.PerformanceAnalysis
     private void GenerateDungeon()
     {
-        foreach (var f in floors)
+        foreach (var floor in floors)
         {
-            int pick = Random.Range(0, f.Count);
-            dungeon.Add(f[pick]);
-            Debug.Log("Room " + f[pick] + " has been added to the dungeon.");
+            int pick = Random.Range(0, floor.Count);
+            dungeon.Add(floor[pick]);
         }
     }
     
-    // ReSharper disable Unity.PerformanceAnalysis
-    private void RestartGeneration()
+    //TODO will be called by a collider in the future
+    public void GoToNextFloor()
     {
-        Debug.Log("Restarting dungeon generation...");
-        dungeon.Clear();
-        GenerateDungeon();
+        curentFloor++;
+
+        if (curentFloor <= dungeon.Count)
+        {
+            SceneManager.LoadScene(dungeon[curentFloor]);
+        }
+        else
+        {
+            Debug.Log("Fin du donjon, il n'existe plus aucun étage accessible.");
+        }
     }
 }
