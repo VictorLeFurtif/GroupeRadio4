5fdba3026902e8b7478ad594776f949b962b7c75
G-Villeroy <villeroy.gab@gmail.com>
2025-04-27 23:59:48 +0200

    MainMenu
    Continue panel
    Options panel
    Quit function
    dummy buttons (future proofed in code)

--- ProjectRadioGroup4/Assets/Scripts/UI/MainMenuUI.cs
+++ ProjectRadioGroup4/Assets/Scripts/UI/MainMenuUI.cs
@@ -1,12 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class MainMenuUI : MonoBehaviour
 {
+    //[SerializeField] private Image newGameButtonImage;
+    //[SerializeField] private Image continueButtonImage;
+    //[SerializeField] private Image optionsButtonImage;
+    //[SerializeField] private Image quitButtonImage;
+    //[SerializeField] private Image onSelectNewGameButtonImage;
+    //[SerializeField] private Image onSelectContinueButtonImage;
+    //[SerializeField] private Image onSelectOptionsButtonImage;
+    //[SerializeField] private Image onSelectQuitButtonImage;
+    
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject continueScreenPanel;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private TMP_Text completionPercentageText;
+    [SerializeField] private TMP_Text playTimeText;
+    private void Update()
+    {
+        //PlayTimeText = 
+        //CompletionPercentageText = 
+    }
+
     public void NewGame()
     {
         
@@ -14,12 +38,26 @@ public class MainMenuUI : MonoBehaviour
 
     public void Continue()
     {
-        
+        continueScreenPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
     }
 
-    public void Options()
+    public void OpenOptions()
     {
-        
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void QuitOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void QuitLoadScreen()
+    {
+        continueScreenPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 
     public void Quit()
