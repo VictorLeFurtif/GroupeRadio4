0780706579d73511866ec0ea67df0013268d32fb
Victor <victor.baz92@gmail.com>
2025-06-09 10:12:15 +0200

    fix menu + fid Idle + fix square out of world offsett

--- ProjectRadioGroup4/Assets/Scripts/UI/MainMenuUI.cs
+++ ProjectRadioGroup4/Assets/Scripts/UI/MainMenuUI.cs
@@ -1,14 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using MANAGER;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
-using Image = UnityEngine.UI.Image;
+
+
 
 public class MainMenuUI : MonoBehaviour
 {
@@ -17,22 +14,91 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject optionsPanel;
     [SerializeField] private GameObject continueScreenPanel;
     [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private TMP_Text completionPercentageText;
-    [SerializeField] private TMP_Text playTimeText;
+    [SerializeField] private TMP_Text displayText;
+    [SerializeField, TextArea(3, 10)] private List<string> textSequence;
+    [SerializeField] private float typingSpeed = 0.05f;
+    [SerializeField] private float delayBetweenTexts = 1f;
+    [SerializeField] private GameObject storyLineOb;
+
+    private Coroutine currentSequence;
+    private string targetSceneName;
+    private bool isTyping = false;
+    
+    private AsyncOperation sceneLoadingOperation;
+    
     private void Start()
     {
         ChangeWindowMode(true);
-        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (isTyping && Input.GetKeyDown(KeyCode.Space))
+        {
+            SkipIntro();
+        }
     }
 
     public void LoadGameByNameScene(string _nameScene)
     {
-        SceneManager.LoadScene(_nameScene);
-        if (_nameScene == "LV1" && NewPlayerController.instance != null)
+        if (currentSequence != null)
+            StopCoroutine(currentSequence);
+        
+        targetSceneName = _nameScene;
+        currentSequence = StartCoroutine(TextSequenceCoroutine(_nameScene));
+    }
+
+    private IEnumerator TextSequenceCoroutine(string sceneName)
+    {
+        storyLineOb.SetActive(true);
+        isTyping = true;
+
+        foreach (string text in textSequence)
+        {
+            yield return StartCoroutine(TypeTextCoroutine(text));
+            yield return new WaitForSeconds(delayBetweenTexts);
+        }
+
+        FinishSequence(sceneName);
+    }
+
+    private IEnumerator TypeTextCoroutine(string text)
+    {
+        displayText.text = "";
+        
+        foreach (char letter in text.ToCharArray())
+        {
+            displayText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private void FinishSequence(string sceneName)
+    {
+        isTyping = false;
+        SceneManager.LoadScene(sceneName);
+        
+        if (sceneName == "LV1" && NewPlayerController.instance != null)
         {
             NewPlayerController.instance.transform.position = new Vector3(6, 0, 0);
         }
+        
+        storyLineOb.SetActive(false);
     }
+
+    private void SkipIntro()
+    {
+        if (currentSequence != null)
+            StopCoroutine(currentSequence);
+        
+        if (textSequence.Count > 0 && displayText != null)
+        {
+            displayText.text = textSequence[^1];
+        }
+        
+        FinishSequence(targetSceneName);
+    }
+
 
     public void Continue()
     {
